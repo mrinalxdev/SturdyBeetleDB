532340a53ad60d35bab13b2fd522c7c66935c379
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-13 23:01:59 +0530

    Added Data in mockData.js

---
+++ src/data/mockData.js
@@ -0,0 +1,50 @@
+export const mockDataTeam = [
+    {
+        id : 1,
+        name : "Mrinal Pramanick",
+        email : "mrinaldemo@gmail.com",
+        age : 35,
+        phone : "78230-123-44",
+        access : "admin"
+    },
+    {
+        id : 2,
+        name : "Mrinal Pramanick",
+        email : "mrinaldemo@gmail.com",
+        age : 35,
+        phone : "78230-123-44",
+        access : "manager"
+    },
+    {
+        id : 3,
+        name : "Mrinal Pramanick",
+        email : "mrinaldemo@gmail.com",
+        age : 35,
+        phone : "78230-123-44",
+        access : "co-manager"
+    },
+    {
+        id : 4,
+        name : "Mrinal Pramanick",
+        email : "mrinaldemo@gmail.com",
+        age : 35,
+        phone : "78230-123-44",
+        access : "admin"
+    },
+    {
+        id : 5,
+        name : "Mrinal Pramanick",
+        email : "mrinaldemo@gmail.com",
+        age : 35,
+        phone : "78230-123-44",
+        access : "admin"
+    },
+    {
+        id : 1,
+        name : "Mrinal Pramanick",
+        email : "mrinaldemo@gmail.com",
+        age : 35,
+        phone : "78230-123-44",
+        access : "admin"
+    },
+]
