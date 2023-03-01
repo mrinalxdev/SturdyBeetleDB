6e13519486f60b543cb30773e526b6a73ea6e94e
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-02 02:13:53 +0530

    new

--- src/scenes/global/Sidebar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Sidebar = () => {
-  return (
-    <div>This is Sidebar for the Admin </div>
-  )
-}
-
-export default Sidebar
--- src/scenes/global/topbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const topbar = () => {
-  return (
-    <div>topbar</div>
-  )
-}
-
-export default topbar
