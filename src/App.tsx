9ad7c66b21cd771231b382d734d6b5b4fc240913
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-03 00:20:53 +0530

    New Changeas

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from 'react-bootstrap'
+import {Routes, Route, Navigate} from "react-router-dom"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+    <Container className='my-4'>
+      <Routes>
+        <Route path = '/' element = {<h1>Home</h1>}/>
+        <Route path = '/new' element = { <NewNote /> }/>
+        <Route path='/:id' element={<Navigate to="/" />}>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+    
   )
 }
 
