7dc4c2b449280243ad45d4f9feef2990cdc43c20
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-05 03:47:23 +0530

    Added New Note Logic with custom Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useMemo } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from 'react-bootstrap'
 import {Routes, Route, Navigate} from "react-router-dom"
 import NewNote from './NewNote'
+import {v4 as uuidV4} from "uuid"
 
 import { useLocalStorage } from './useLocalStorage'
 
@@ -36,13 +38,25 @@ function App() {
 
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  function onCreateNote( { tags, ...data }: NoteData){
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidV4(), tagsIds: tags.map(tag => tag.id) }]
+    })
+  }
  
   return (
 
     <Container className='my-4'>
       <Routes>
         <Route path = '/' element = {<h1>Home</h1>}/>
-        <Route path = '/new' element = { <NewNote /> }/>
+        <Route path = '/new' element = { <NewNote onSubmit={onCreateNote} /> }/>
         <Route path='/:id' element={<Navigate to="/" />}>
           <Route index element={<h1>Show</h1>} />
           <Route path='edit' element={<h1>Edit</h1>} />
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import NoteForm from './NoteForm'
 
-const NewNote = () => {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+}
+
+const NewNote = ({onSubmit} : NewNoteProps) => {
   return (
     <> 
         <h1 className="mb-4">New Note</h1>
