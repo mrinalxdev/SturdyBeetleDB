aae50a20ad31d47bf8cfdb55a72b5d5f6a486ed3
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-11-05 00:20:21 +0530

    Added New Data

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center h-screen flex justify-center items-center text-2xl">This is Landing Page</div>
+    </>
+  );
+}
+
+export default App;
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export type Id = string | number
-
-export type Column = {
-    id : Id,
-    title : string,
-
-}
