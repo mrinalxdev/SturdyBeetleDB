02b00c9f8872e27502acefca6856f5d2b6eb3772
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-11-05 01:13:37 +0530

    Added Basic Data

---
+++ src/data.js
@@ -0,0 +1,46 @@
+import LogoImg from "../src/assets/header/logo.svg";
+import HeroImg from "../src/assets/hero/image.svg";
+import OverviewProductImg from "../src/assets/overview/product.svg";
+import FacebookImg from "../src/assets/overview/brands/facebook.svg";
+import GoogleImg from "../src/assets/overview/brands/google.svg";
+import CocaColaImg from "../src/assets/overview/brands/coca-cola.svg";
+import Feature1Img from "../src/assets/features/feature1-img.svg";
+import Feature2Img from "../src/assets/features/feature2-img.svg";
+import Feature3Img from "../src/assets/features/feature3-img.svg";
+import ArrowRightImg from "../src/assets/features/arrow-right.svg";
+import CardIconImg1 from "../src/assets/product/cards/icon1.svg";
+import CardIconImg2 from "../src/assets/product/cards/icon2.svg";
+import CardIconImg3 from "../src/assets/product/cards/icon3.svg";
+import PricingIcon1 from "../src/assets/pricing/icon1.svg";
+import PricingIcon2 from "../src/assets/pricing/icon2.svg";
+import PricingIcon3 from "../src/assets/pricing/icon3.svg";
+import AvatarImg1 from "../src/assets/testimonial/avatar1.png";
+import AvatarImg2 from "../src/assets/testimonial/avatar2.png";
+import AvatarImg3 from "../src/assets/testimonial/avatar3.png";
+import AvatarImg4 from "../src/assets/testimonial/avatar4.png";
+import AvatarImg5 from "../src/assets/testimonial/avatar5.png";
+import CtaImg1 from "../src/assets/cta/image1.svg";
+import CtaImg2 from "../src/assets/cta/image2.svg";
+import FacebookIcon from "../src/assets/copyright/facebook.svg";
+import TwitterIcon from "../src/assets/copyright/twitter.svg";
+import LinkedinIcon from "../src/assets/copyright/linkedin.svg";
+
+export const header = {
+  logo: LogoImg,
+  btnText: "Request a Demo",
+};
+
+export const nav = [
+  { name: "Features", href: "/" },
+  { name: "About Us", href: "/" },
+  { name: "Pricing", href: "/" },
+  { name: "Feedback", href: "/" },
+];
+
+export const hero =  {
+    title : 'Track your Expenses',
+    subtitle : 'Helps you to organize your income and expenses',
+    btnText : 'Try free demo',
+    compText : '- Web, IOS and Android',
+    image : HeroImg
+}
