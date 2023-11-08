60ec5d4e1efbb27bd1a581bc161df2f35e1f6052
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-11-09 00:42:47 +0530

    Added Aos : animate on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
 
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Overview from "./components/Overview";
+import Brands from "./components/Brands";
+import Feature1 from "./components/Feature1";
+import Feature2 from "./components/Feature2";
+import Feature3 from "./components/Feature3";
+import Product from "./components/Product";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Cta from "./components/Cta";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <div className="text-center h-screen flex justify-center items-center text-2xl">This is Landing Page</div>
+      <div className="overflow-hidden">
+        <Header />
+        <Hero />
+        <Overview />
+        <Brands />
+        <Feature1 />
+        <Feature2 />
+        <Feature3 />
+        <Product />
+        <Pricing />
+        <Testimonials />
+        <Cta />
+        <Footer />
+        {/* <div className='h-[4000px]'></div> */}
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Hind Siliguri",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        lg: "30px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1170px",
+    },
+    extend: {
+      colors: {
+        dark: "#292830",
+        light: "#BDBDBD",
+        accent: "#FF7235",
+        accentHover: "#e05216",
+        grey: "#F5F5F5",
+      },
+      backgroundImage: {
+        overview: "url('/src/assets/img/overview/bg.svg')",
+        cta: "url('/src/assets/img/cta/bg.svg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
