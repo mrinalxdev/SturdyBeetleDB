f9cb99794bf3c920f260d09cafbead95b20141fb
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-11-27 20:48:12 +0530

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors : {
+        primary : "#1D5B79",
+        secondary : "#2EC0BB",
+        heading : "#233036",
+        body : "#667074",
+        light : "#F6F6F6",
+        yellowLight : "#FFF3D9",
+        greenLight : "#CCFOEE",
+        purpleLight : "#AEA2AE1"
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
